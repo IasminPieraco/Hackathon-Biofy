9b49c4caa8b5a12c9f497d618ce45317b905b44b
IasminPieraco <iasminpieraco.rodrigues@hotmail.com>
2024-03-16 17:09:48 -0300

    parte 2

--- Hackathon_Biofy/app/src/main/java/com/example/hackathon_biofy/MainActivity3.kt
+++ Hackathon_Biofy/app/src/main/java/com/example/hackathon_biofy/MainActivity3.kt
@@ -3,10 +3,11 @@ package com.example.hackathon_biofy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_main3)
 
     }
 }
--- Hackathon_Biofy/app/src/main/java/com/example/hackathon_biofy/MainActivity4.kt
+++ Hackathon_Biofy/app/src/main/java/com/example/hackathon_biofy/MainActivity4.kt
@@ -3,10 +3,11 @@ package com.example.hackathon_biofy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_main4)
 
     }
 }
