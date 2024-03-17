cd4f75dfb0235ff6e12670fb59c27cb83df83d29
Bruno Hiago Xavier <brunohiagoxavier@outlook.com>
2024-03-17 16:22:43 -0300

    site finalizado

--- front-end/script.js
+++ front-end/script.js
@@ -1,16 +1,62 @@
 
 
+const api = axios.create({
+    baseURL: 'http://localhost:3000'
+});
+
+function abrirMenu(num) {
+    var menu = document.getElementById("menu");
+    var preve = document.getElementById("preve");
+    var cancel = document.getElementById("cancel");
+    var dias = document.getElementById("dias");
+    var data = document.getElementById("data");
+    menu.style.display = "block";
+
+    preve.addEventListener("click", async function () {
+        switch (num) {
+            case 1:
+                var previsao = "V";
+                break;
+            case 2:
+                var previsao = "UV";
+                break;
+            case 3:
+                var previsao = "A";
+                break;
+            case 4:
+                var previsao = "A";
+                break;
+        }
+
+        menu.style.display = "none";
+        let medicao =  api.get(`previsao${previsao}/`+ num)
+            .then(response => {
+                return response.data;
+            });
+        let matriz =  api.get(`/previsoes${previsao}/ `+ num)
+            .then(response => {
+                return response.data;
+            });
+        abrirModal(num);
+    });
+
+    cancel.addEventListener("click", function () {
+        menu.style.display = "none";
+    });
+}
+
+
 function abrirModal(num) {
     var modal = document.getElementById("modal" + num);
     var modais = document.getElementsByClassName('modal');
     var globo = document.getElementById("globo");
 
     for (var i = 0; i < modais.length; i++) {
-        if(num -1 != i){
+        if (num - 1 != i) {
             modais[i].style.display = "none";
         }
     }
-    
+
     if (modal.style.display !== "block") {
         globo.style.display = "none";
         modal.style.display = "block";
@@ -19,43 +65,32 @@ function abrirModal(num) {
         modal.style.display = "none";
     }
 
-    const xArray = [50,60,70,80,90,100,110,120,130,140,150];
-        const yArray = [7,8,8,9,9,9,10,11,14,14,15];
-        
-        // Define Data
-        const data = [{
-          x: xArray,
-          y: yArray,
-          mode:"lines",
-          line:{color:'blue'}
-        }];
-        
-        // Define Layout
-        const layout = {
-          xaxis: {range: [40, 160], title: "Dias", tickfont: {color: 'white'}, titlefont:{color:"white"}},
-          yaxis: {range: [0, 20], title: "Price in Millions", tickfont: {color: 'white'}, titlefont:{color:"white"}},  
-          title: "",
-          titlefont:{color:"white"},
-          plot_bgcolor: 'rgba(255,255,255,0.2)', // Define o fundo do gráfico como transparente
-         paper_bgcolor: 'rgba(0,0,0,0)',
-         legend: {font: {color: 'white'}} // Define a cor da legenda
-        };
-        
-        // Display using Plotly
-        switch(num){
-            case 1:
-                Plotly.newPlot("ar", data, layout);
-                break;
-            case 2:
-                Plotly.newPlot("fogo", data, layout);
-                break;
-            case 3:
-                Plotly.newPlot("terra", data, layout);
-                break;
-            case 4:
-                Plotly.newPlot("agua", data, layout);
-                break;
-        }
+    const xArray = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const yArray = [7, 8, 8, 9, 9, 10, 11, 12, 13, 14, 15];
+    // Define Data
+    const data = [{
+        x: xArray,
+        y: yArray,
+        mode: "lines",
+        line: { color: 'blue' }
+    }];
+
+    // Define Layout
+    const layout = {
+        xaxis: { range: [5, 16], title: "Dias", tickfont: { color: 'white' }, titlefont: { color: "white" } },
+        yaxis: { range: [0, 20], title: "Previsão", tickfont: { color: 'white' }, titlefont: { color: "white" } },
+        title: "",
+        titlefont: { color: "white" },
+        plot_bgcolor: 'rgba(255,255,255,0.2)', // Define o fundo do gráfico como transparente
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        legend: { font: { color: 'white' } } // Define a cor da legenda
+    };
+
+    
+    Plotly.newPlot("ar", data, layout);
+    Plotly.newPlot("fogo", data, layout);
+    Plotly.newPlot("terra", data, layout);
+    Plotly.newPlot("agua", data, layout);
 }
 
 function fecharModal(idModal) {
